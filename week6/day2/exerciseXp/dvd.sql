a2cdf15350f1c3bf0c034046247285a4d66b1754
Guilbert21 <guilbertalexis534@gmail.com>
2023-12-05 22:19:35 +0400

    Update .DS_Store file

---
+++ week6/day2/exerciseXp/dvd.sql
@@ -0,0 +1,20 @@
+-- exercise1
+-- SELECT * FROM customers;
+-- SELECT * FROM item;
+
+-- All items, ordered by price (lowest to highest).
+-- SELECT * FROM item ORDER BY price DESC;
+
+-- Items with a price above 80 (80 included), ordered by price (highest to lowest).
+-- SELECT * FROM ITEM WHERE price > 80 ORDER BY price DESC;
+
+-- The first 3 customers in alphabetical order of the first name (A-Z) – exclude the primary key column from the results.
+-- SELECT * FROM customers ORDER BY firstname DESC LIMIT 3;
+
+-- All last names (no other columns!), in reverse alphabetical order (Z-A)
+-- SELECT lastname FROM customers ORDER BY firstname ASC;
+
+
+-- exercise2
+
+
---
+++ week6/day2/paraWorking/paraWoking.sql
@@ -0,0 +1,20 @@
+-- SELECT * FROM actors
+
+-- SELECT first_name AS name_actor, age AS birth_date FROM actors;
+
+-- SELECT avg(number_oscars) AS average_number_oscars FROM actors;
+
+-- SELECT DISTINCT number_oscars FROM actors;
+
+-- SELECT * FROM actors WHERE age > '01/01/1970'
+
+-- SELECT * FROM actors WHERE first_name IN ('Matt', 'Morgan')
+
+
+-- UPDATE actors set first_name = 'maty' WHERE first_name = 'Matt'
+-- UPDATE actors set number_oscars = 4 WHERE first_name = 'George';
+-- ALTER TABLE actors RENAME COLUMN age TO birthdate;
+-- DELETE FROM actors WHERE actor_id=1;
+
+-- SELECT * FROM actors
+
