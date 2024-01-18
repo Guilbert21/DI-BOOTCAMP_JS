63e87886ee96e4f4e68516e85d652a8ae8e5e5cc
Guilbert21 <guilbertalexis534@gmail.com>
2024-01-18 20:39:43 +0400

    Remove non-letter characters from input value

--- week3/day3/daily-challenge/script.js
+++ week3/day3/daily-challenge/script.js
@@ -1,8 +1,6 @@
 document.getElementById('letterInput').addEventListener('input', function(event) {
-    // Get the input value and remove non-letter characters using regular expression
     let inputValue = event.target.value;
     let onlyLetters = inputValue.replace(/[^a-zA-Z]/g, '');
     
-    // Update the input value with only letters
     event.target.value = onlyLetters;
   });
